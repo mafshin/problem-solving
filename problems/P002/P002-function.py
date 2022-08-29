dec82adfd6578ddf4050646485493b1d2dcb934c
Hossein2468 <hosseinbaziepic@gmail.com>
2022-08-29 12:58:20 +0430

    added P002

---
+++ problems/P002/P002-function.py
@@ -0,0 +1,14 @@
+a = int(input("enter the first number:"))
+b = int(input("enter the second number:"))
+def my_sum(x , y): 
+    print (x + y)
+def my_subtract(x , y):
+    print (y - x) 
+def my_multiply(x , y):
+    print (x * y) 
+def my_divide(x , y):
+    print (y / x)
+my_sum(a , b)
+my_subtract(a , b)
+my_multiply(a , b)
+my_divide(a , b)
