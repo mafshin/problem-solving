b31e95aca9e7913ad503007b53929223ae9e92ae
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-10 14:41:54 +0430

    added P020

---
+++ problems/P020/P020-numbers.py
@@ -0,0 +1,3 @@
+a = input("Enter the number: ")
+b = {"1" : "One" , "2" : "Two" , "3" : "Three" , "4" : "Four" , "5" : "Five" , "6" : "Six" , "7" : "Seven" , "8" : "Eight" , "9" : "Nine" , "10" : "Ten"}
+print (b[a])
