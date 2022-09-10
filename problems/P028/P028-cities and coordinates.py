b1d1a1c69da081099ada1b4fa13065be46d198f3
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-10 14:49:20 +0430

    nothing added

---
+++ problems/P028/P028-cities and coordinates.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number of cities: "))
+for m in range(n):
+    m = input("Enter the city and Coordinates: ")
+o = ""
+while o != "END" :
+    o = input("Enter the cities: ")
