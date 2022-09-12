c1de17e46b6e4fff8728a55e14e280dbb3223c8c
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-12 13:32:13 +0430

    added P027

--- problems/P027/P027-huffman and dictionary.py
+++ problems/P027/P027-huffman and dictionary.py
@@ -1,26 +1,34 @@
 a = str(input("Enter the string: "))
 
 if a.startswith("P") :
-    def encode(a):
-        b = (a[2:]).split()
-        list1 = []
-        for x in range(len(b)):
-            y = x , b[x]
-            list1.append(x)
-            print(y)
-        print(list1)
+    def encode(a): 
+        list1 = ['A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z' , 'a' , 'b' , 'c' ,'d' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' , '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9']
+        list2 = []
+        b = a[2:].split()
+        c = set(b)
+        for d in c :
+            list2.append(d)
+        e = list1[:len(c)]
+        print(e)
+        i = 0 
+        while i < len(c) :
+            print(f'{list2[i]} : {e[i]}')
+            i += 1
 
 if a.startswith("E") :
     def decode(a):
-        list2 = []
-        b = ""
-        while b != "END" :
-            b = input("Enter: ")
-            c = b[1:]
-            list2.append(c)
-        print(list2[:-1])
+        list3 = [] 
+        b = (a[2:].split())
+        c = {}
+        d = ""
+        while d != "END" :
+            d = input("Enter: ")
+            c.update({d[0] : d[1:]})
+        for x in b :
+            list3.append(c[x])
+        print(list3)
 
-if a.startswith("P") :
+if a.startswith("P"): 
     print(encode(a))
-elif a.startswith("E") :
+if a.startswith("E"):
     print(decode(a))
