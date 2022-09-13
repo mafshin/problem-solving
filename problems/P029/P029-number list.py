6e10188a3baa2e7f29b989f4ac3e39831ded6215
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-13 22:06:11 +0430

    nothing added

--- problems/P029/P029-number list.py
+++ problems/P029/P029-number list.py
@@ -2,48 +2,48 @@ m = input("Enter the numbers: ").split()
 
 n = list(map(lambda x : int(x) , m))
 
-a = max(n)
-b = min(n)
+# def min(n):
 
-def sum(l):
-    z = 0 
-    for x in l :
-        z = x + z 
-    return z 
-c = (sum(n) / (len(n)))
+# def max(n):
 
-e = n[-3:]
+def avg(n):
+    def sum(n) :
+        a = 0
+        for b in n :
+            a = b + a
+        return a
+    c = len(n)
+    return (sum(n)) / c
 
-def multiply(l):
-    x = 1 
-    for q in l :
-        x = q * x 
-    return x 
+def last_numbers(n):
+    a = n[-3:]
+    return a
+g = last_numbers(n)
 
-def (l):
-    l.sort()
-    if len(l) % 2 != 0 :
-        j = len(l) / 2 
-        print(l[int(j)])
-    if len(l) % 2 == 0 :
-        h = int(len(l) / 2) 
-        i = h + 1 
-        u = int((l[h] + l[i]) / 2)
-        print(u)
-d = (n)
+def mean(n) :
+    if len(n) % 2 != 0 :
+        n.sort()
+        b = int(len(n) / 2)
+        return n[b]
+    if len(n) % 2 == 0 :
+        n.sort()
+        b = int(len(n) / 2)
+        c = n[b] + n[(b + 1)]
+        return c / 2
 
+# def highest_count(n):
 
-def counter(l):
+def multiply(n) :
+    a = 1 
+    for b in n :
+        a = b * a 
+    return a 
 
-g = sum(n)
-
-h = multiply(n)
-
-print(f"Max is {a}")
-print(f"Min is {b}")
-print(f"Avg is {c}")
-print(f"Mean is {d}")
-print(f"Last 3 numbers are {e}")
-print(f"Two numbers with highest count are")
-print(f"Sum of all is {g}")
-print(f"Product of all is {h}")
+print(f"Max is: ")
+print(f"Min is: ")
+print(f"Avg is: {(avg(n))}")
+print(f"Mean is: {(mean(n))}")
+print(f"Last 3 numbers are: {g}")
+print(f"2 numbers with highest count are: {highest_count(n)}")
+print(f"Sum of all is: {(sum(n))}")
+print(f"Product of all is: {(multiply(n))}")
