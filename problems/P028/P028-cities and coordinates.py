d2a0afe1bfb0eea3e52681ff055dfa26e7051bc5
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-18 11:35:09 +0430

    nothing added

--- problems/P028/P028-cities and coordinates.py
+++ problems/P028/P028-cities and coordinates.py
@@ -1,6 +1,27 @@
 n = int(input("Enter the number of cities: "))
+
+list1 = []
 for m in range(n):
-    m = input("Enter the city and Coordinates: ")
+    m = input("Enter the city and Coordinates: ").split()
+    list1.append(m)
+
+def salam(list1):
+    cities = {}
+    for a in list1 :
+        b = a[1:]
+        c = list(map(lambda x : int(x) , b))
+        cities.update({a[0] : c})
+    return cities 
+
 o = ""
 while o != "END" :
     o = input("Enter the cities: ")
+
+x = salam(list1)
+def hello(x , o):
+    a = 1 
+    while a < len(o):
+        b = o[a]
+        c = o[a - 1]
+
+print(hello(x , o))
