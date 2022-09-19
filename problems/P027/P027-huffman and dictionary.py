d60d3519680c0ee3cff9b65c039ae6bae5a889f8
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-19 14:27:54 +0430

    added P027

--- problems/P027/P027-huffman and dictionary.py
+++ problems/P027/P027-huffman and dictionary.py
@@ -2,24 +2,25 @@ a = str(input("Enter the string: "))
 
 if a.startswith("P") :
     def encode(a): 
-        list1 = ['A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z' , 'a' , 'b' , 'c' ,'d' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' , '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9']
+        list1 = []
         list2 = []
-        list4 = []
         dict1 = {}
-        b = a[2:].split()
+        b = (a[2:].split())
         c = set(b)
         for d in c :
-            list2.append(d)
-        e = list1[:len(c)]
+            list1.append(d)
         i = 0 
         while i < len(c) :
-            dict1.update({list2[i] : e[i]})
-            i += 1
+            for x in range(len(list1)):
+                dict1.update({list1[i] : x})
+                i += 1
         for g in b :
-            list4.append(dict1[g])
-        print(list4)
+            list2.append(dict1[g])
+        y = list(map(lambda x : str(x) , list2))
+        z = ' '.join(y)
+        print(z)
         for h in dict1 :
-            print(f"{h} : {dict1[h]}")
+            print(f"{h}  {dict1[h]}")
 
 if a.startswith("E") :
     def decode(a):
