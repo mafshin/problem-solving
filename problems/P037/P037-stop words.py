0a757a17505621551e21ae694a3f210b8dcf323d
Hossein2468 <hosseinbaziepic@gmail.com>
2022-09-23 18:54:21 +0330

    added P037

--- problems/P037/P037-stop words.py
+++ problems/P037/P037-stop words.py
@@ -1,2 +1,17 @@
-a = "I have no idea"
-print(a)
+a = input("Enter the text: ").split()
+
+list1 = []
+b = " "
+while b != "END" :
+    b = input("Enter: ")
+    list1.append(b)
+list1.remove("END")
+
+for c in list1 :
+    for d in range(a.count(c)):
+        a.remove(c)
+    for e in a :
+        if e == (c + '.') or e == (c + ',') or e == (c + '!') or e == (c + '?') :
+            a.remove(e)
+x = ' '.join(a)
+print(x)
