21e50231d7e3cf03b26df74c9bd61a076e5532ad
Hossein2468 <hosseinbaziepic@gmail.com>
2022-10-13 15:24:25 +0330

    added P037

--- problems/P037/P037-stop words.py
+++ problems/P037/P037-stop words.py
@@ -11,7 +11,7 @@ for c in words :
     for d in range(a.count(c)):
         a.remove(c)
     for e in a :
-        if e == (c + '.') or e == (c + ',') or e == (c + '!') or e == (c + '?') :
+        if e == (c + '.') or e == (c + ',') or e == (c + '!') or e == (c + '?') or e == ('"' + c + '"') or e == ("'" + c + "'") :
             a.remove(e)
 x = ' '.join(a)
 print(x)
