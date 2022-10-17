18576e5f6f212c5f300f7ef15730ce200a8f3f18
Hossein2468 <hosseinbaziepic@gmail.com>
2022-10-17 21:02:21 +0330

    nothing added

--- problems/P039/P039-Common members.py
+++ problems/P039/P039-Common members.py
@@ -1,2 +1,7 @@
-n = "I have no idea"
-print(n)
+n = int(input("Enter the number: "))
+a = []
+for m in range(n):
+    m = input("Enter the numbers list: ").split()
+    o = list(map(lambda x : int(x) , m))
+    a.append(o)
+print(a)
