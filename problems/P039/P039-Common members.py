699e0f81fd9207224fc5bd89709f7dd6bb9def4f
Hossein2468 <hosseinbaziepic@gmail.com>
2022-10-20 19:35:33 +0330

    nothing added

--- problems/P039/P039-Common members.py
+++ problems/P039/P039-Common members.py
@@ -1,7 +1,21 @@
 n = int(input("Enter the number: "))
 a = []
-for m in range(n):
+p = input("Enter the numbers list: ").split()
+q = list(map(lambda x : int(x) , p))
+for m in range(n - 1):
     m = input("Enter the numbers list: ").split()
     o = list(map(lambda x : int(x) , m))
     a.append(o)
-print(a)
+numbers = {'a'}
+b = 0 
+while b < len(q):
+    for d in a :
+        for e in d :
+            c = q[b]
+            if e == c :
+                numbers.add(c)
+                b += 1 
+            else:
+                b += 1
+numbers.remove('a')
+print(numbers)
